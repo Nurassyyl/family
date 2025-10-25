3efdfdf8af1b1e1a45fb543318204edf5e4fc31e
Nurassyyl <93340696+Nurassyyl@users.noreply.github.com>
2025-10-25 14:27:03 +0500

    Update menu.js

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,6 +1,5 @@
 const windowInnerWidth = window.innerWidth;
 const body = document.body;
-// const overflowHidden = document.querySelector(".overflow-hidden");
 const menuBtnOpen = document.querySelector(".nav-menu-menu");
 const menuBtnClose = document.querySelector(".close-menu-btn");
 const menuBtnCloseTablet = document.querySelector(".close-menu-btn-tablet");
@@ -9,17 +8,15 @@ const menuDesktop = document.querySelectorAll(".bg-transparent");
 const menuTablet = document.querySelector(".modal-menu-tablet");
 const menuPhone = document.querySelector(".modal-menu-phone");
 
-console.log(menuDesktop[1]);
-
 
 if (windowInnerWidth > 769) {
           menuBtnOpen.addEventListener('click', () => {
-                    menuDesktop.style.display = "block";
+                    menuDesktop[0].style.display = "block";
                     body.classList.add("overflow-hidden");
           })
 
           menuBtnClose.addEventListener('click', () => {
-                    menuDesktop.style.display = "none";
+                    menuDesktop[0].style.display = "none";
                     body.classList.remove("overflow-hidden");
           })
 } else if (windowInnerWidth > 480) {
@@ -49,6 +46,7 @@ if (windowInnerWidth > 769) {
           })
 
 }
+
 
 
 
